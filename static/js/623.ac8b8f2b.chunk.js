298bd80e69318ed7eea769da5fcb2cb8b60de42d
MarcoMaya098 <Marco.maya098@gmail.com>
2025-04-17 00:33:02 -0700

    Updates

---
+++ static/js/623.ac8b8f2b.chunk.js
@@ -0,0 +1,45 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[623],{7623:(t,e,i)=>{i.r(e),i.d(e,{default:()=>z});var n=i(5043);const o=JSON.parse('{"DD":"titleIntroContent","Qq":"titleIntroContentText","x6":[{"title":"ExplorerServices","toredirect":"basicServices"},{"title":"Learn more","color":"#fff","toredirect":"about"}]}'),r=JSON.parse('{"DD":"titleDescriptionContent","Qq":"titleDescriptionContentText"}'),s=JSON.parse('{"DD":"titleBasicServiceContent","Qq":"titleBasicServiceContentText"}'),c=JSON.parse('{"D":"Mission","Q":"titleMissionContent"}'),l=JSON.parse('{"D":"Vision","Q":"titleVisionContent"}'),d=JSON.parse('{"D":"titleSpecialServiceContent","Q":"titleSpecialServiceContentText"}'),a=JSON.parse('{"D":"titleProductContent","Q":"titleProductContentText"}'),p=JSON.parse('{"DD":"titleVisitContent","Qq":"titleVisitContentText","uW":[{"title":"visitTitleSectionFirstText","content":"visitContentSectionFirstTextMF","content2":"visitContentSectionFirstTextSat"},{"title":"visitTileSectionSecondText","content":"Av. GuanaJuato 2713. Col. Madero(Cacho), 22040, Tijuana. B.C, Local 3","content2":""}]}');var x=i(4647),h=i(1319),m=i(3614),j=i(9281),u=i(5639),g=i(2646),v=i(9);const f=(0,v.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,y=(0,v.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,D=(0,v.Ay)(h.A)`
+  flex-direction: ${t=>{let{direction:e}=t;return"left"===e?"row":"row-reverse"}};
+`,C=(0,v.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,S=(0,v.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,b=(0,v.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,w=(0,v.Ay)("p")`
+  font-size: 13px;
+`,A=(0,v.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`;var Q=i(579);const T=(0,j.C)()((t=>{let{icon:e,title:i,content:n,section:o,button:r,t:s,id:c,direction:l}=t;return(0,Q.jsx)(f,{children:(0,Q.jsx)(m.zW,{direction:l,triggerOnce:!0,children:(0,Q.jsxs)(D,{justify:"space-between",align:"middle",id:c,direction:l,children:[(0,Q.jsx)(x.A,{lg:11,md:11,sm:12,xs:24,children:(0,Q.jsx)(g.A,{src:e,width:"100%",height:"100%"})}),(0,Q.jsx)(x.A,{lg:11,md:11,sm:11,xs:24,children:(0,Q.jsxs)(C,{children:[(0,Q.jsx)("h6",{children:s(i)}),(0,Q.jsx)(y,{children:s(n)}),"right"===l?(0,Q.jsx)(A,{children:"object"===typeof r&&r.map(((t,e)=>(0,Q.jsx)(u.$,{color:t.color,onClick:()=>(t=>{document.getElementById(t).scrollIntoView({behavior:"smooth"})})("about"),children:s(t.title)},e)))}):(0,Q.jsx)(S,{children:(0,Q.jsx)(h.A,{justify:"space-between",children:"object"===typeof o&&o.map(((t,e)=>{const i=s(t.content),n=s(t.content2),o=i.includes("Av.")||i.includes("Col.")||i.includes("Tijuana");return(0,Q.jsxs)(x.A,{span:11,children:[(0,Q.jsx)(b,{children:s(t.title)}),o?(0,Q.jsx)(w,{children:(0,Q.jsx)("a",{href:`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(i)}`,target:"_blank",rel:"noopener noreferrer",children:i})}):(0,Q.jsx)(w,{children:i}),n&&(0,Q.jsx)(w,{children:n})]},e)}))})})]})})]})})})})),q=(0,n.lazy)((()=>i.e(416).then(i.bind(i,9416)))),J=(0,n.lazy)((()=>Promise.resolve().then(i.bind(i,1766)))),O=(0,n.lazy)((()=>i.e(538).then(i.bind(i,8538)))),N=(0,n.lazy)((()=>i.e(566).then(i.bind(i,6566)))),z=()=>(0,Q.jsxs)(J,{children:[(0,Q.jsx)(O,{}),(0,Q.jsx)(N,{direction:"right",title:o.DD,content:o.Qq,button:o.x6,icon:"landing.png",id:"intro"}),(0,Q.jsx)(q,{title:r.DD,content:r.Qq}),(0,Q.jsx)(N,{direction:"left",title:s.DD,content:s.Qq,icon:"mascarilla.png",id:"basicServices"}),(0,Q.jsx)(N,{direction:"right",title:d.D,content:d.Q,icon:"mision.png",id:"specialServices"}),(0,Q.jsx)(N,{direction:"left",title:a.D,content:a.Q,icon:"producto.png",id:"product"}),(0,Q.jsx)("hr",{}),(0,Q.jsx)(T,{direction:"left",title:p.DD,content:p.Qq,section:p.uW,icon:"visit.png",id:"about"}),(0,Q.jsx)(N,{direction:"left",title:c.D,content:c.Q,icon:"about.png",id:"vision"}),(0,Q.jsx)(N,{direction:"right",title:l.D,content:l.Q,icon:"vision.png",id:"vision"})]})}}]);
+//# sourceMappingURL=623.ac8b8f2b.chunk.js.map
