20d83729130aeb3df0717c3b5a41857d8eb9340d
MarcoMaya098 <Marco.maya098@gmail.com>
2025-04-17 14:13:54 -0700

    Updates

---
+++ static/js/566.f790e73a.chunk.js
@@ -0,0 +1,45 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[566],{6566:(e,i,t)=>{t.r(i),t.d(i,{default:()=>w});var n=t(4647),r=t(1319),s=t(3614),d=t(9281),c=t(5639),o=t(2646),l=t(9);const a=(0,l.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 2rem 0 2rem;
+  }
+`,h=(0,l.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,x=(0,l.Ay)(r.A)`
+  flex-direction: ${e=>{let{direction:i}=e;return"left"===i?"row":"row-reverse"}};
+`,m=(0,l.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,p=(0,l.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,j=(0,l.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,y=(0,l.Ay)("p")`
+  font-size: 13px;
+`,f=(0,l.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`;var g=t(579);const w=(0,d.C)()((e=>{let{icon:i,title:t,content:d,section:l,button:w,t:u,id:A,direction:b}=e;return(0,g.jsx)(a,{style:{marginBottom:"30px"},children:(0,g.jsx)(s.zW,{direction:b,triggerOnce:!0,children:(0,g.jsxs)(x,{justify:"space-between",align:"middle",id:A,direction:b,children:[(0,g.jsx)(n.A,{lg:11,md:11,sm:11,xs:24,children:(0,g.jsxs)(m,{children:[(0,g.jsx)("h6",{children:u(t)}),(0,g.jsx)(h,{children:u(d)}),"right"===b?(0,g.jsx)(f,{children:"object"===typeof w&&w.map(((e,i)=>(0,g.jsx)(c.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})(e.toredirect),children:u(e.title)},i)))}):(0,g.jsx)(p,{children:(0,g.jsx)(r.A,{justify:"space-between",children:"object"===typeof l&&l.map(((e,i)=>(0,g.jsxs)(n.A,{span:11,children:[(0,g.jsx)(o.A,{src:e.icon,width:"60px",height:"60px"}),(0,g.jsx)(j,{children:u(e.title)}),(0,g.jsx)(y,{children:u(e.content)})]},i)))})})]})}),(0,g.jsx)(n.A,{lg:11,md:11,sm:12,xs:24,children:(0,g.jsx)(o.A,{src:i,width:"100%",height:"100%"})})]})})})}))}}]);
+//# sourceMappingURL=566.f790e73a.chunk.js.map
