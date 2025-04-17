80e097e959927c9e2bfa788ddeac658e19d27044
MarcoMaya098 <Marco.maya098@gmail.com>
2025-04-17 11:45:41 -0700

    Updates

---
+++ static/js/416.290b8417.chunk.js
@@ -0,0 +1,20 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[416],{9416:(e,n,i)=>{i.r(n),i.d(n,{default:()=>m});var t=i(1319),d=i(4647),r=i(9281),s=i(3614),c=i(9);const a=(0,c.Ay)("section")`
+  position: relative;
+  padding: 7.5rem 0 3rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 1024px) {
+    padding: 5.5rem 0 3rem;
+  }
+`,l=(0,c.Ay)("p")`
+  padding: 0.75rem 0 0.75rem;
+`,x=(0,c.Ay)("div")`
+  max-width: 570px;
+
+  @media only screen and (max-width: 768px) {
+    max-width: 100%;
+  }
+`;var h=i(579);const m=(0,r.C)()((e=>{let{title:n,content:i,t:r}=e;return(0,h.jsx)(a,{children:(0,h.jsx)(s.q7,{direction:"up",triggerOnce:!0,children:(0,h.jsx)(t.A,{justify:"center",align:"middle",children:(0,h.jsx)(x,{children:(0,h.jsxs)(d.A,{lg:24,md:24,sm:24,xs:24,children:[(0,h.jsx)("h6",{children:r(n)}),(0,h.jsx)(l,{children:r(i)})]})})})})})}))}}]);
+//# sourceMappingURL=416.290b8417.chunk.js.map
