bdca50c91f3531cce00ef5fd71e70390ac19200e
MarcoMaya098 <Marco.maya098@gmail.com>
2025-04-17 17:18:53 -0700

    Updates

--- static/js/566.db2b0a69.chunk.js
+++ static/js/566.db2b0a69.chunk.js
@@ -3,7 +3,7 @@
   padding: 10rem 0 8rem;
 
   @media only screen and (max-width: 1024px) {
-    padding: 2rem 0 2rem;
+    padding: 1rem 0 1rem;
   }
 `,h=(0,l.Ay)("p")`
   margin: 1.5rem 0 2rem 0;
@@ -14,7 +14,7 @@
   max-width: 540px;
 
   @media only screen and (max-width: 575px) {
-    padding-top: 4rem;
+    padding-top: 1rem;
   }
 `,p=(0,l.Ay)("div")`
   display: flex;
@@ -42,4 +42,4 @@
     margin-left: 20px;
   }
 `;var g=t(579);const w=(0,d.C)()((e=>{let{icon:i,title:t,content:d,section:l,button:w,t:u,id:A,direction:b}=e;return(0,g.jsx)(a,{style:{marginBottom:"30px"},children:(0,g.jsx)(s.zW,{direction:b,triggerOnce:!0,children:(0,g.jsxs)(x,{justify:"space-between",align:"middle",id:A,direction:b,children:[(0,g.jsx)(n.A,{lg:11,md:11,sm:11,xs:24,children:(0,g.jsxs)(m,{children:[(0,g.jsx)("h6",{children:u(t)}),(0,g.jsx)(h,{children:u(d)}),"right"===b?(0,g.jsx)(f,{children:"object"===typeof w&&w.map(((e,i)=>(0,g.jsx)(c.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})(e.toredirect),children:u(e.title)},i)))}):(0,g.jsx)(p,{children:(0,g.jsx)(r.A,{justify:"space-between",children:"object"===typeof l&&l.map(((e,i)=>(0,g.jsxs)(n.A,{span:11,children:[(0,g.jsx)(o.A,{src:e.icon,width:"60px",height:"60px"}),(0,g.jsx)(j,{children:u(e.title)}),(0,g.jsx)(y,{children:u(e.content)})]},i)))})})]})}),(0,g.jsx)(n.A,{lg:11,md:11,sm:12,xs:24,children:(0,g.jsx)(o.A,{src:i,width:"100%",height:"100%"})})]})})})}))}}]);
-//# sourceMappingURL=566.f790e73a.chunk.js.map
+//# sourceMappingURL=566.db2b0a69.chunk.js.map
---
+++ static/js/843.e4acfe14.chunk.js
@@ -0,0 +1,130 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[843],{3843:(e,t,i)=>{i.r(t),i.d(t,{default:()=>X});var n=i(5043);const r=JSON.parse('{"DD":"titleIntroContent","Qq":"titleIntroContentText","x6":[{"title":"ExplorerServices","toredirect":"basicServices"},{"title":"Learn more","color":"#fff","toredirect":"visit"}]}'),s=JSON.parse('{"DD":"titleDescriptionContent","Qq":"titleDescriptionContentText"}'),o=JSON.parse('{"DD":"titleBasicServiceContent","Qq":"titleBasicServiceContentText"}'),c=JSON.parse('{"D":"Mission","Q":"titleMissionContent"}'),d=JSON.parse('{"D":"Vision","Q":"titleVisionContent"}'),l=JSON.parse('{"D":"titleSpecialServiceContent","Q":"titleSpecialServiceContentText"}'),a=JSON.parse('{"D":"titleProductContent","Q":"titleProductContentText"}'),x=JSON.parse('{"DD":"titleVisitContent","Qq":"titleVisitContentText","uW":[{"title":"visitTitleSectionFirstText","content":"visitContentSectionFirstTextMF","content2":"visitContentSectionFirstTextSat"},{"title":"visitTileSectionSecondText","content":"Av. GuanaJuato 2713. Col. Madero(Cacho), 22040, Tijuana. B.C, Local 3","content2":""}]}');var h=i(4647),p=i(1319),m=i(3614),j=i(9281),y=i(5639),f=i(2646),g=i(9);const u=(0,g.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 6rem 0 2rem;
+  }
+`,w=(0,g.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,v=(0,g.Ay)(p.A)`
+  flex-direction: ${e=>{let{direction:t}=e;return"left"===t?"row":"row-reverse"}};
+`,A=(0,g.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,b=(0,g.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,C=(0,g.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,S=(0,g.Ay)("p")`
+  font-size: 13px;
+`,D=(0,g.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`;var Q=i(579);const T=(0,j.C)()((e=>{let{icon:t,title:i,content:n,section:r,button:s,t:o,id:c,direction:d}=e;return(0,Q.jsx)(u,{children:(0,Q.jsx)(m.zW,{direction:d,triggerOnce:!0,children:(0,Q.jsxs)(v,{justify:"space-between",align:"middle",id:c,direction:d,children:[(0,Q.jsx)(h.A,{lg:11,md:11,sm:12,xs:24,children:(0,Q.jsx)(f.A,{src:t,width:"100%",height:"100%"})}),(0,Q.jsx)(h.A,{lg:11,md:11,sm:11,xs:24,children:(0,Q.jsxs)(A,{children:[(0,Q.jsx)("h6",{children:o(i)}),(0,Q.jsx)(w,{children:o(n)}),"right"===d?(0,Q.jsx)(D,{children:"object"===typeof s&&s.map(((e,t)=>(0,Q.jsx)(y.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})("about"),children:o(e.title)},t)))}):(0,Q.jsx)(b,{children:(0,Q.jsx)(p.A,{justify:"space-between",children:"object"===typeof r&&r.map(((e,t)=>{const i=o(e.content),n=o(e.content2),r=i.includes("Av.")||i.includes("Col.")||i.includes("Tijuana");return(0,Q.jsxs)(h.A,{span:11,children:[(0,Q.jsx)(C,{children:o(e.title)}),r?(0,Q.jsx)(S,{children:(0,Q.jsx)("a",{href:`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(i)}`,target:"_blank",rel:"noopener noreferrer",children:i})}):(0,Q.jsx)(S,{children:i}),n&&(0,Q.jsx)(S,{children:n})]},t)}))})})]})})]})})})})),z=(0,g.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 4rem 0 4rem;
+  }
+`,O=(0,g.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,k=(0,g.Ay)(p.A)`
+  flex-direction: ${e=>{let{direction:t}=e;return"left"===t?"row":"row-reverse"}};
+`,q=(0,g.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 0rem;
+  }
+`,I=(0,g.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,J=(0,g.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,N=(0,g.Ay)("p")`
+  font-size: 13px;
+`,M=(0,g.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`,V=(0,j.C)()((e=>{let{icon:t,title:i,content:n,section:r,button:s,t:o,id:c,direction:d}=e;return(0,Q.jsx)(z,{children:(0,Q.jsx)(m.zW,{direction:d,triggerOnce:!0,children:(0,Q.jsxs)(k,{justify:"space-between",align:"middle",id:c,direction:d,children:[(0,Q.jsx)(h.A,{lg:11,md:11,sm:11,xs:24,children:(0,Q.jsxs)(q,{children:[(0,Q.jsx)("h6",{children:o(i)}),(0,Q.jsx)(O,{children:o(n)}),"left"===d?(0,Q.jsx)(M,{children:"object"===typeof s&&s.map(((e,t)=>(0,Q.jsx)(y.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})(e.toredirect),children:o(e.title)},t)))}):(0,Q.jsx)(I,{children:(0,Q.jsx)(p.A,{justify:"space-between",children:"object"===typeof r&&r.map(((e,t)=>(0,Q.jsxs)(h.A,{span:11,children:[(0,Q.jsx)(f.A,{src:e.icon,width:"60px",height:"60px"}),(0,Q.jsx)(J,{children:o(e.title)}),(0,Q.jsx)(N,{children:o(e.content)})]},t)))})})]})}),(0,Q.jsx)(h.A,{lg:11,md:11,sm:12,xs:24,style:{marginTop:"30px"},children:(0,Q.jsx)(f.A,{src:t,width:"100%",height:"100%"})})]})})})})),$=(0,g.Ay)("section")`
+  position: relative;
+  padding: 10rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 2rem 0 2rem;
+  }
+`,B=(0,g.Ay)("p")`
+  margin: 1.5rem 0 2rem 0;
+`,L=(0,g.Ay)(p.A)`
+  flex-direction: ${e=>{let{direction:t}=e;return"left"===t?"row":"row-reverse"}};
+`,W=(0,g.Ay)("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-top: 4rem;
+  }
+`,E=(0,g.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+`,F=(0,g.Ay)("h6")`
+  font-size: 15px;
+  line-height: 1rem;
+  padding: 0.5rem 0;
+  text-transform: uppercase;
+  color: #000;
+  font-family: "Motiva Sans Light", sans-serif;
+`,P=(0,g.Ay)("p")`
+  font-size: 13px;
+`,G=(0,g.Ay)("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+  button:last-child {
+    margin-left: 20px;
+  }
+`,R=(0,j.C)()((e=>{let{icon:t,title:i,content:n,section:r,button:s,t:o,id:c,direction:d}=e;return(0,Q.jsx)($,{children:(0,Q.jsx)(m.zW,{direction:d,triggerOnce:!0,children:(0,Q.jsxs)(L,{justify:"space-between",align:"middle",id:c,direction:d,children:[(0,Q.jsx)(h.A,{lg:11,md:11,sm:12,xs:24,children:(0,Q.jsx)(f.A,{src:t,width:"100%",height:"100%"})}),(0,Q.jsx)(h.A,{lg:11,md:11,sm:11,xs:24,children:(0,Q.jsxs)(W,{children:[(0,Q.jsx)("h6",{children:o(i)}),(0,Q.jsx)(B,{children:o(n)}),"right"===d?(0,Q.jsx)(G,{children:"object"===typeof s&&s.map(((e,t)=>(0,Q.jsx)(y.$,{color:e.color,onClick:()=>(e=>{document.getElementById(e).scrollIntoView({behavior:"smooth"})})(e.toredirect),children:o(e.title)},t)))}):(0,Q.jsx)(E,{children:(0,Q.jsx)(p.A,{justify:"space-between",children:"object"===typeof r&&r.map(((e,t)=>(0,Q.jsxs)(h.A,{span:11,children:[(0,Q.jsx)(f.A,{src:e.icon,width:"60px",height:"60px"}),(0,Q.jsx)(F,{children:o(e.title)}),(0,Q.jsx)(P,{children:o(e.content)})]},t)))})})]})})]})})})})),U=(0,n.lazy)((()=>i.e(416).then(i.bind(i,9416)))),_=(0,n.lazy)((()=>Promise.resolve().then(i.bind(i,1766)))),H=(0,n.lazy)((()=>i.e(538).then(i.bind(i,8538)))),K=(0,n.lazy)((()=>i.e(566).then(i.bind(i,6566)))),X=()=>(0,Q.jsxs)(_,{children:[(0,Q.jsx)(H,{}),(0,Q.jsx)(V,{direction:"left",title:r.DD,content:r.Qq,button:r.x6,icon:"landing.png",id:"intro"}),(0,Q.jsx)(U,{title:s.DD,content:s.Qq}),(0,Q.jsx)(K,{direction:"left",title:o.DD,content:o.Qq,icon:"mascarilla.png",id:"basicServices"}),(0,Q.jsx)(K,{direction:"right",title:l.D,content:l.Q,icon:"mision.png",id:"specialServices"}),(0,Q.jsx)(K,{direction:"left",title:a.D,content:a.Q,icon:"producto.png",id:"product"}),(0,Q.jsx)("hr",{id:"visit"}),(0,Q.jsx)(T,{direction:"left",title:x.DD,content:x.Qq,section:x.uW,icon:"moani-background.jpeg",id:"about"}),(0,Q.jsx)(R,{direction:"left",title:c.D,content:c.Q,icon:"about.png",id:"mission"}),(0,Q.jsx)(R,{direction:"right",title:d.D,content:d.Q,icon:"vision.png",id:"vision"})]})}}]);
+//# sourceMappingURL=843.e4acfe14.chunk.js.map
